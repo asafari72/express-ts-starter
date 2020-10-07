67251d5c71a04c1507c10751e6e24abd82eff997
asafari72 <asafari72@gmail.com>
2020-10-07 14:03:28 +0330

    fix vccode debugger

--- dist/utils/util.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.isEmptyObject = void 0;
-exports.isEmptyObject = (obj) => {
-    return !Object.keys(obj).length;
-};
-//# sourceMappingURL=util.js.map
